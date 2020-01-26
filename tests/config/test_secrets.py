848d9bc72ca9de8102ce0a5d99299b995295a837
Jim Fawkes <meilfort@protonmail.com>
2020-01-26 22:13:35 +0100

    Add EnvLoader and CredentialLoader Classes
     - Add description for current v0.2 idea/concept
     - Add tests
     - Few notes with untested/unannotated code

--- tests/config/test_secrets.py
+++ tests/config/test_secrets.py
@@ -1,6 +1,314 @@
 import pytest
+import os
 
 from utils.config import secrets
+
+
+# ----------------------------------------------------------------------------
+# Test BaseClass
+# ----------------------------------------------------------------------------
+
+
+def test_base_class_has_repr():
+    base_class = secrets.BaseClass()
+
+    assert "BaseClass()" == str(base_class)
+
+
+# ----------------------------------------------------------------------------
+# Test BaseLoader
+# ----------------------------------------------------------------------------
+
+
+def test_base_loader_exists():
+    base_loader = secrets.BaseLoader()
+
+    assert base_loader is not None
+    assert isinstance(base_loader, secrets.BaseLoader)
+
+
+def test_base_loader_has_load_method():
+    base_loader = secrets.BaseLoader()
+
+    with pytest.raises(NotImplementedError):
+        base_loader.load("SOME_VAR")
+
+
+def test_base_loader_load_method_requires_attribute():
+    base_loader = secrets.BaseLoader()
+
+    with pytest.raises(TypeError):
+        base_loader.load()
+
+
+def test_base_loader_has_repr():
+    base_loader = secrets.BaseLoader()
+
+    assert "BaseLoader(" in str(base_loader)
+
+
+# ----------------------------------------------------------------------------
+# Test DumyLoader
+# ----------------------------------------------------------------------------
+
+
+class DummyLoader(secrets.BaseLoader):
+    def __init__(self, raise_not_found=False):
+        self.raise_not_found = raise_not_found
+
+    def load(self, credential_name):
+
+        if self.raise_not_found:
+            raise secrets.CredentialNotFoundError(f"DummyLoader raise_not_found=True")
+
+        if credential_name is None:
+            raise secrets.CredentialNotFoundError(f"DummyLoader encountered a None Value")
+
+        return credential_name
+
+
+def test_dummy_loader_exists():
+    dummy_loader = DummyLoader()
+
+    assert dummy_loader is not None
+    assert isinstance(dummy_loader, DummyLoader)
+
+
+def test_dummy_loader_load_variable():
+    dummy_loader = DummyLoader()
+    dummy_var_name = "MY_TEST_ENV_VAR"
+
+    value = dummy_loader.load(dummy_var_name)
+
+    assert value == dummy_var_name
+
+
+def test_dummy_loader_fail_for_none():
+    dummy_loader = DummyLoader()
+
+    with pytest.raises(secrets.CredentialNotFoundError):
+        value = dummy_loader.load(None)
+
+
+def test_dummy_loader_fail_if_forced():
+    dummy_loader = DummyLoader(raise_not_found=True)
+
+    with pytest.raises(secrets.CredentialNotFoundError) as exception_info:
+        value = dummy_loader.load("some_var")
+
+    assert "raise_not_found" in str(exception_info.value)
+
+
+# ----------------------------------------------------------------------------
+# Test EnvLoader
+# ----------------------------------------------------------------------------
+
+
+def test_env_loader_exists():
+    env_loader = secrets.EnvLoader()
+
+    assert env_loader is not None
+    assert isinstance(env_loader, secrets.EnvLoader)
+
+
+def test_env_loader_load_env_variable():
+    env_loader = secrets.EnvLoader()
+
+    env_var_name = "MY_TEST_ENV_VAR"
+    env_var_value = "some_value"
+    os.environ[env_var_name] = env_var_value
+
+    value = env_loader.load(env_var_name)
+
+    # Remove side-effects for other tests
+    # TODO: Check if there is a better way to do this
+    del os.environ[env_var_name]
+
+    assert value == env_var_value
+
+
+def test_env_loader_fail_for_none_existing_variable():
+    env_loader = secrets.EnvLoader()
+
+    env_var_name = "MY_TEST_ENV_VAR"
+
+    with pytest.raises(secrets.CredentialNotFoundError):
+        value = env_loader.load(env_var_name)
+
+
+# The default value should only be used after checking all loaders
+# Implement on CredentialLoader returned by Factory
+# def test_env_loader_use_default_for_none_existing_variable():
+#     env_loader = secrets.EnvLoader()
+#
+#     env_var_name = "MY_TEST_ENV_VAR"
+#     default_env_var = "some_default_value"
+#
+#     value = env_loader.load(env_var_name, default=default_env_var)
+#     assert value == default_env_var
+
+# ----------------------------------------------------------------------------
+# Test EnvFileLoader
+# ----------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------
+# Test AWSSecretsManagerLoader
+# ----------------------------------------------------------------------------
+
+
+# ----------------------------------------------------------------------------
+# Test CredentialLoaderFactory
+# ----------------------------------------------------------------------------
+
+
+def test_credential_loader_exists():
+    cred = secrets.CredentialLoader()
+    assert cred is not None
+
+
+def test_credential_loader_is_callable():
+    cred = secrets.CredentialLoader()
+    assert callable(cred)
+
+
+def test_credential_loader_pass_empty_loaders():
+    loaders = []
+    cred = secrets.CredentialLoader(loaders=loaders)
+
+    assert cred.loaders == loaders
+
+
+def test_credential_loader_pass_dummy_loader_as_dict():
+    loaders = [
+        {"name": "DummyLoader", "loader": DummyLoader, "args": (), "kwargs": {"raise_not_found": True},},
+    ]
+    cred = secrets.CredentialLoader(loaders=loaders)
+
+    assert isinstance(cred.loaders[0], DummyLoader)
+    assert cred.loaders[0].raise_not_found == True
+
+
+def test_credential_loader_pass_dummy_loader_as_callable():
+    loaders = [DummyLoader]
+    cred = secrets.CredentialLoader(loaders=loaders)
+
+    assert isinstance(cred.loaders[0], DummyLoader)
+    assert cred.loaders[0].raise_not_found == False
+
+
+def test_credential_loader_pass_dummy_loader_as_tuple():
+    loaders = [
+        ("DummyLoader", DummyLoader, (), {"raise_not_found": True}),
+    ]
+    cred = secrets.CredentialLoader(loaders=loaders)
+
+    assert isinstance(cred.loaders[0], DummyLoader)
+    assert cred.loaders[0].raise_not_found == True
+
+
+def test_credential_loader_construct_loader_list():
+    loaders = [
+        ("DummyLoader", DummyLoader, (), {"raise_not_found": True}),
+    ]
+    cred = secrets.CredentialLoader(loaders=[])
+    loader_list = cred._construct_loader_list(loaders)
+
+    assert isinstance(loader_list[0], DummyLoader)
+    assert loader_list[0].raise_not_found == True
+
+
+def test_credential_loader_construct_loader_list_fail_for_wrong_type():
+    loaders = [DummyLoader()]
+    cred = secrets.CredentialLoader(loaders=[])
+
+    with pytest.raises(secrets.ConstructLoaderError):
+        loader_list = cred._construct_loader_list(loaders)
+
+
+def test_credential_loader_construct_loader():
+    loader_name = "DummyLoader"
+    loader_class = DummyLoader
+    kwargs = {"raise_not_found": True}
+
+    cred = secrets.CredentialLoader(loaders=[])
+    loader = cred._construct_loader(name=loader_name, loader=loader_class, **kwargs)
+
+    assert isinstance(loader, DummyLoader)
+    assert loader.raise_not_found == True
+
+
+def test_credential_loader_with_dummy_loader():
+    loaders = [DummyLoader]
+    cred = secrets.CredentialLoader(loaders=loaders)
+    dummy_credential = "some_cred"
+
+    assert dummy_credential == cred(dummy_credential)
+
+
+def test_credential_loader_empty_loaders_raises_no_loaders():
+    loaders = []
+    cred = secrets.CredentialLoader(loaders=loaders)
+
+    with pytest.raises(secrets.NoLoaderConfiguredError):
+        cred("SOME_DUMMY_CREDENTIAL")
+
+
+def test_credential_loader_raises_not_found_after_last_loader_failed():
+    loaders = [
+        ("DummyLoader", DummyLoader, (), {"raise_not_found": True}),
+    ]
+    cred = secrets.CredentialLoader(loaders=loaders)
+    credential_name = "SOME_DUMMY_CREDENTIAL"
+
+    with pytest.raises(secrets.CredentialNotFoundError) as excption_info:
+        cred(credential_name)
+
+    assert f"Could not load '{credential_name}' using loaders: [" in str(excption_info.value)
+
+
+# TODO: Review if this makes sense and if DFL should be tested for its structure
+# NOTE: Review this test, delete DEFAULT_LOADERS if test is not added again
+# def test_credential_loader_has_default_loaders():
+#     cred = secrets.CredentialLoader()
+#     assert secrets.DEFAULT_LOADERS == cred.loaders
+
+
+def test_credential_laoder_register_loader():
+    loaders = []
+    cred = secrets.CredentialLoader(loaders=loaders)
+    loader = DummyLoader
+    cred.register(name="DummyLoader", loader=loader)
+
+    assert isinstance(cred.loaders[0], DummyLoader)
+    assert len(cred.loaders) == 1
+
+
+def test_credential_laoder_register_loader_with_kwargs():
+    loaders = []
+    cred = secrets.CredentialLoader(loaders=loaders)
+    loader = DummyLoader
+    cred.register(name="DummyLoader", loader=loader, raise_not_found=True)
+
+    assert isinstance(cred.loaders[0], DummyLoader)
+    assert len(cred.loaders) == 1
+    assert cred.loaders[0].raise_not_found == True
+
+
+def test_credential_laoder_register_loader_order():
+    cred = secrets.CredentialLoader()
+
+    assert len(cred.loaders) == len(secrets.DEFAULT_LOADERS)
+
+    cred.register(name="dummy", loader=DummyLoader)
+
+    assert len(cred.loaders) == len(secrets.DEFAULT_LOADERS) + 1
+    assert isinstance(cred.loaders[0], DummyLoader)
+
+
+# ----------------------------------------------------------------------------
+# Test Credential
+# ----------------------------------------------------------------------------
 
 
 def test_credential_exists():
@@ -123,121 +431,121 @@ def test_credential_iter():
 # ----------------------------------------------------------------------------
 # Test Credentials
 # ----------------------------------------------------------------------------
-
-
-def test_credentials_load_single_from_dict():
-    secret_name = "SecretName"
-    secret_value = "SecretValue"
-    secrets_dict = {
-        secret_name: secret_value,
-    }
-    creds = secrets.Credentials()
-    assert 1 == creds.load(secrets_dict)
-
-
-def test_credentials_load_multiple_from_dict():
-    secrets_dict = {
-        "SecretName": "SecretValue",
-        "SecondSecretName": "SecondSecretValue",
-    }
-    creds = secrets.Credentials()
-    assert 2 == creds.load(secrets_dict)
-
-
-def test_credentials_load_none_from_dict():
-    secrets_dict = {}
-    creds = secrets.Credentials()
-    assert 0 == creds.load(secrets_dict)
-
-
-def test_credentials_get_credential():
-    secret_name = "SecretName"
-    secret_value = "SecretValue"
-    secrets_dict = {
-        secret_name: secret_value,
-    }
-    creds = secrets.Credentials()
-    creds.load(secrets_dict)
-
-    assert creds.get(secret_name) == secret_value
-
-
-def test_credentials_len():
-    secrets_dict = {
-        "SecretName": "SecretValue",
-    }
-    creds_1 = secrets.Credentials()
-    creds_1.load(secrets_dict)
-    assert len(creds_1) == 1
-
-    secrets_dict["SecondSecretName"] = "SecondSecret"
-    creds_2 = secrets.Credentials()
-    creds_2.load(secrets_dict)
-    assert len(creds_2) == 2
-
-
-def test_credentials_getitem():
-    secret_name = "SecretName"
-    secret_value = "SecretValue"
-    secrets_dict = {secret_name: secret_value, "SecondSecretName": "SecondSecretValue"}
-    creds = secrets.Credentials()
-    creds.load(secrets_dict)
-
-    assert creds[secret_name] == secret_value
-
-
-def test_credentials_getitem_unknown_item():
-    secret_name = "SecretName"
-    secret_value = "SecretValue"
-    secrets_dict = {secret_name: secret_value, "SecondSecretName": "SecondSecretValue"}
-    creds = secrets.Credentials()
-    creds.load(secrets_dict)
-
-    with pytest.raises(KeyError):
-        creds["ThirdSecretName"]
-
-
-def test_credentials_iter():
-    secrets_dict = {
-        "SecretName": "SecretValue",
-        "SecondSecretName": "SecondSecretValue",
-    }
-
-    creds = secrets.Credentials()
-    creds.load(secrets_dict)
-
-    for secret_name in creds:
-        assert secret_name in secrets_dict.keys()
-
-
-def test_credentials_load_string_secret():
-    creds = secrets.Credentials()
-
-    with pytest.raises(NotImplementedError):
-        creds.load("SOME_STRING_SECRET")
-
-
-def test_credentials_load_list_secret():
-    creds = secrets.Credentials()
-
-    with pytest.raises(NotImplementedError):
-        creds.load(["SECRET_NAME_1", "SECRET_NAME_2"])
-
-
-def test_credentials_load_tuple_secret():
-    creds = secrets.Credentials()
-
-    with pytest.raises(NotImplementedError):
-        creds.load(("SECRET_NAME_1", "SECRET_NAME_2"))
-
-
-def test_credentials_load_none_as_secret_value():
-    creds = secrets.Credentials()
-    with pytest.raises(secrets.CredentialMutabilityError):
-        creds.load(
-            {"SECRET_NAME": None,}
-        )
-
-
+#
+#
+# def test_credentials_load_single_from_dict():
+#     secret_name = "SecretName"
+#     secret_value = "SecretValue"
+#     secrets_dict = {
+#         secret_name: secret_value,
+#     }
+#     creds = secrets.Credentials()
+#     assert 1 == creds.load(secrets_dict)
+#
+#
+# def test_credentials_load_multiple_from_dict():
+#     secrets_dict = {
+#         "SecretName": "SecretValue",
+#         "SecondSecretName": "SecondSecretValue",
+#     }
+#     creds = secrets.Credentials()
+#     assert 2 == creds.load(secrets_dict)
+#
+#
+# def test_credentials_load_none_from_dict():
+#     secrets_dict = {}
+#     creds = secrets.Credentials()
+#     assert 0 == creds.load(secrets_dict)
+#
+#
+# def test_credentials_get_credential():
+#     secret_name = "SecretName"
+#     secret_value = "SecretValue"
+#     secrets_dict = {
+#         secret_name: secret_value,
+#     }
+#     creds = secrets.Credentials()
+#     creds.load(secrets_dict)
+#
+#     assert creds.get(secret_name) == secret_value
+#
+#
+# def test_credentials_len():
+#     secrets_dict = {
+#         "SecretName": "SecretValue",
+#     }
+#     creds_1 = secrets.Credentials()
+#     creds_1.load(secrets_dict)
+#     assert len(creds_1) == 1
+#
+#     secrets_dict["SecondSecretName"] = "SecondSecret"
+#     creds_2 = secrets.Credentials()
+#     creds_2.load(secrets_dict)
+#     assert len(creds_2) == 2
+#
+#
+# def test_credentials_getitem():
+#     secret_name = "SecretName"
+#     secret_value = "SecretValue"
+#     secrets_dict = {secret_name: secret_value, "SecondSecretName": "SecondSecretValue"}
+#     creds = secrets.Credentials()
+#     creds.load(secrets_dict)
+#
+#     assert creds[secret_name] == secret_value
+#
+#
+# def test_credentials_getitem_unknown_item():
+#     secret_name = "SecretName"
+#     secret_value = "SecretValue"
+#     secrets_dict = {secret_name: secret_value, "SecondSecretName": "SecondSecretValue"}
+#     creds = secrets.Credentials()
+#     creds.load(secrets_dict)
+#
+#     with pytest.raises(KeyError):
+#         creds["ThirdSecretName"]
+#
+#
+# def test_credentials_iter():
+#     secrets_dict = {
+#         "SecretName": "SecretValue",
+#         "SecondSecretName": "SecondSecretValue",
+#     }
+#
+#     creds = secrets.Credentials()
+#     creds.load(secrets_dict)
+#
+#     for secret_name in creds:
+#         assert secret_name in secrets_dict.keys()
+#
+#
+# def test_credentials_load_string_secret():
+#     creds = secrets.Credentials()
+#
+#     with pytest.raises(NotImplementedError):
+#         creds.load("SOME_STRING_SECRET")
+#
+#
+# def test_credentials_load_list_secret():
+#     creds = secrets.Credentials()
+#
+#     with pytest.raises(NotImplementedError):
+#         creds.load(["SECRET_NAME_1", "SECRET_NAME_2"])
+#
+#
+# def test_credentials_load_tuple_secret():
+#     creds = secrets.Credentials()
+#
+#     with pytest.raises(NotImplementedError):
+#         creds.load(("SECRET_NAME_1", "SECRET_NAME_2"))
+#
+#
+# def test_credentials_load_none_as_secret_value():
+#     creds = secrets.Credentials()
+#     with pytest.raises(secrets.CredentialMutabilityError):
+#         creds.load(
+#             {"SECRET_NAME": None,}
+#         )
+#
+#
 # TODO:
 #     - Reload behaviour
--- utils/config/secrets.py
+++ utils/config/secrets.py
@@ -1,18 +1,138 @@
 """
+v0.1
 This module defines all interactions with sensitive credentials.
 Specifically it defines:
     1. A container to store a single key/value pair [Credential]
     2. A container to store several key/value pairs [Credentials] (Review naming)
     3. Machinery to load credentials from different sources
+------------------------------------------------------------------------------
+v0.2
+Usage:
+    # config.py in some config file
+    credential = CredentialLoaderFactory(
+                    search_order=[
+                        secrets.DictLoader,
+                        secrets.EnvLoader,
+                        secrets.EnvFileLoader,
+                        secrets.AWSSecretsManagerLoader
+                    ],
+                    raise_failure=True
+                ) # pass in other config
+
+    api_password = credential("API_PASSWORD", default=None, raise_failure=False)
+     - Try to laod credential in all places according to search order
+     - For version v0.2 return plain text value, later maybe return
+        Credential Object and allow reload or expiration or MetaData, maybe allow
+        configuration in the loader to return plain text or credential object
+
+    env_loader = EnvLoader(prefix="") # The prefix allways adds a prefix to the value name,
+                                      # e.g., VALUE_NAME => AWS_VALUE_NAME with prefix="AWS_"
+                                      # maybe in a later version enable case switch lower => capital
+    value = env_loader.load("VALUE_NAME")
+
+    DictLoader ?? How to deal with multiple secrets? Postpone dict loader for now
+    EnvFileLoader(env_file, env_path) use py env loader
+
+
+    Think about giving the factory a register method to register custom loaders.
+    There could also be an attr to allow to ignore the rest (potential race-condition?)
+
 """
+
 import warnings
+import os
 
 from collections.abc import Mapping
-from typing import Union, Generator
+from typing import Union, Generator, Any, List, Callable
+
+
+class CredentialNotFoundError(Exception):
+    """Could not load Credential"""
+
+
+class NoLoaderConfiguredError(Exception):
+    """No Loader was provided"""
+
+
+class ConstructLoaderError(Exception):
+    """Could not construct loader"""
 
 
 class CredentialMutabilityError(Exception):
     """Encountered a mutable type"""
+
+
+class BaseClass:
+    def __repr__(self):
+        attributes = ", ".join([f"{key}={value}" for key, value in vars(self).items()])
+        return f"{self.__class__.__name__}({attributes})"
+
+
+class BaseLoader(BaseClass):
+    def load(self, credential_name: str) -> str:
+        raise NotImplementedError(f"A Loader needs to implement load(credential_name)")
+
+
+class EnvLoader(BaseLoader):
+    def load(self, credential_name: str) -> str:
+        value = os.environ.get(credential_name)
+        if value is None:
+            raise CredentialNotFoundError(f"EnvLoader could not load {credential_name}")
+        return value
+
+
+# Should the default list, be a global var?
+# Should the default list contain instances or classes that get instatiated?
+# NOTE: NOT TESTED
+# TODO: Refactor and/or write tests
+DEFAULT_LOADERS = [EnvLoader]
+
+
+# TODO: Think about renaming this class
+class CredentialLoader(BaseClass):
+    def __init__(self, loaders: list = DEFAULT_LOADERS) -> None:
+        self.loaders: List[BaseLoader] = self._construct_loader_list(loaders)
+
+    def __call__(self, credential_name: str) -> str:
+        if not self.loaders:
+            raise NoLoaderConfiguredError(f"{self} has no loader configured, loaders={self.loaders}")
+        for loader in self.loaders:
+            try:
+                credential: str = loader.load(credential_name)
+                return credential
+            except CredentialNotFoundError as e:
+                continue
+
+        raise CredentialNotFoundError(f"Could not load '{credential_name}' using loaders: {self.loaders}")
+
+    # TODO: FIXME: loader has no type annotation, I am not sure which type to use
+    @staticmethod
+    def _construct_loader(name: str, loader, *args: Any, **kwargs: Any) -> BaseLoader:
+        return loader(*args, **kwargs)
+
+    @staticmethod
+    def _construct_loader_list(loaders: Union[list, tuple]) -> list:
+        loader_list: list = []
+        for loader in loaders:
+            if callable(loader):
+                loader_list.append(CredentialLoader._construct_loader("", loader))
+            elif isinstance(loader, dict):
+                loader_list.append(
+                    CredentialLoader._construct_loader(
+                        loader["name"], loader["loader"], *loader["args"], **loader["kwargs"]
+                    )
+                )
+            elif isinstance(loader, tuple):
+                name, loader_, args, kwargs = loader
+                loader_list.append(CredentialLoader._construct_loader(name, loader_, *args, **kwargs))
+            else:
+                raise ConstructLoaderError(f"Could not construct loader for {loader}")
+
+        return loader_list
+
+    def register(self, name: str, loader: BaseLoader, *args: Any, **kwargs: Any) -> None:
+        constructed_loader = self._construct_loader(name, loader, *args, **kwargs)
+        self.loaders.insert(0, constructed_loader)
 
 
 class Credential(Mapping):
@@ -63,38 +183,38 @@ class Credential(Mapping):
         return self._secret
 
 
-class Credentials(Mapping):
-    """Store a list of credentials and provide the machinery to load them."""
-
-    # TODO: Review keys(), items(), values(), get()
-    # TODO: Review if getitem should return Credential or the revealed secret
-
-    def __init__(self) -> None:
-        self._secrets: dict = {}
-
-    def __getitem__(self, key: str) -> Union[int, str]:
-        return self._secrets[key].reveal()
-
-    def __iter__(self) -> Generator[str, None, None]:
-        for key in self._secrets.keys():
-            yield key
-
-    def __len__(self) -> int:
-        return len(self._secrets)
-
-    def __repr__(self):
-        return f"Credentials()"
-
-    def load(self, secret: dict) -> Union[int]:
-        if not isinstance(secret, dict):
-            raise NotImplementedError(f"Currently only supporting dicts")
-
-        if not secret:
-            return 0
-
-        inserted_secrets: int = 0
-        for key, value in secret.items():
-            self._secrets[key] = Credential(name=key, secret=value)
-            inserted_secrets += 1
-
-        return inserted_secrets
+# class Credentials(Mapping):
+#     """Store a list of credentials and provide the machinery to load them."""
+#
+#     # TODO: Review keys(), items(), values(), get()
+#     # TODO: Review if getitem should return Credential or the revealed secret
+#
+#     def __init__(self) -> None:
+#         self._secrets: dict = {}
+#
+#     def __getitem__(self, key: str) -> Union[int, str]:
+#         return self._secrets[key].reveal()
+#
+#     def __iter__(self) -> Generator[str, None, None]:
+#         for key in self._secrets.keys():
+#             yield key
+#
+#     def __len__(self) -> int:
+#         return len(self._secrets)
+#
+#     def __repr__(self):
+#         return f"Credentials()"
+#
+#     def load(self, secret: dict) -> Union[int]:
+#         if not isinstance(secret, dict):
+#             raise NotImplementedError(f"Currently only supporting dicts")
+#
+#         if not secret:
+#             return 0
+#
+#         inserted_secrets: int = 0
+#         for key, value in secret.items():
+#             self._secrets[key] = Credential(name=key, secret=value)
+#             inserted_secrets += 1
+#
+#         return inserted_secrets
