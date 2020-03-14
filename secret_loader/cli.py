cff10bae525b75881c33acb468b82bb1c54bfbb5
Jim Fawkes <meilfort@protonmail.com>
2020-03-14 15:48:52 +0100

    Add loader arg to cli to select a specific loader

--- secret_loader/cli.py
+++ secret_loader/cli.py
@@ -17,7 +17,13 @@ Version v0.1 - March 2020 - Jim Fawkes - src: github.com/JimFawkes/secret-loader
 
 import argparse
 
-from secret_loader.secrets import secret, SecretNotFoundError
+from secret_loader import secrets
+
+# This is depneding on all available default loaders. Might need a refactor if
+# not all possible loaders are registered by default.
+available_loaders = {
+    loader.loader_class.__name__: loader.loader_class for loader in secrets.secret.loaders
+}
 
 parser = argparse.ArgumentParser(
     prog="secret_loader",
@@ -32,11 +38,26 @@ parser.add_argument(
 
 parser.add_argument("--fail", help="Fail if Secret is not Found", action="store_true")
 
+parser.add_argument(
+    "--loader", help="Specify a Loader to use", choices=available_loaders.keys(),
+)
+
+
+def get_secret_loader(args):
+    if args.loader:
+        secret = secrets.SecretLoader()
+        secret.register(available_loaders[args.loader])
+        return secret
+
+    else:
+        return secrets.secret
+
 
 def secret_loader_cli(args):
+    secret = get_secret_loader(args)
     try:
         print(secret(args.name))
-    except SecretNotFoundError as e:
+    except secrets.SecretNotFoundError as e:
         if args.fail:
             raise e
         else:
--- tests/secret_loader/test_cli.py
+++ tests/secret_loader/test_cli.py
@@ -4,6 +4,13 @@ from unittest.mock import patch
 from secret_loader import cli, secrets
 
 
+class MockArgs:
+    def __init__(self, name, fail=False, loader=None, **kwargs):
+        self.name = name
+        self.fail = fail
+        self.loader = loader
+
+
 def test_argument_parser_exits_for_missing_required_args():
     with pytest.raises(SystemExit) as e:
         args = cli.parser.parse_args()
@@ -30,16 +37,35 @@ def test_argument_parser_sets_arg_fail_as_true_when_provided():
     assert args.fail is True
 
 
-@patch("secret_loader.cli.secret")
+def test_argument_parser_sets_arg_loader_as_none_by_default():
+    secret_name = "SOME_NAME"
+    args = cli.parser.parse_args(["--name", f"{secret_name}"])
+
+    assert args.loader is None
+
+
+def test_argument_parser_exits_for_unknown_loader():
+    secret_name = "SOME_NAME"
+    unknown_loader = "no_real_loader"
+
+    with pytest.raises(SystemExit) as e:
+        cli.parser.parse_args(["--name", secret_name, "--loader", unknown_loader])
+
+
+def test_argument_parser_accepts_valid_loader():
+    secret_name = "SOME_NAME"
+    valid_loader = list(cli.available_loaders)[0]
+
+    args = cli.parser.parse_args(["--name", secret_name, "--loader", valid_loader])
+
+    assert args.loader == valid_loader
+
+
+@patch("secret_loader.secrets.secret")
 def test_secret_loader_cli(mock_secret, capsys):
     secret_name = "secret_name"
     secret_value = "secret_value"
     mock_secret.return_value = secret_value
-
-    class MockArgs:
-        def __init__(self, name, fail=False):
-            self.name = name
-            self.fail = fail
 
     args = MockArgs(secret_name)
     cli.secret_loader_cli(args)
@@ -50,16 +76,11 @@ def test_secret_loader_cli(mock_secret, capsys):
     assert cleaned_output == secret_value
 
 
-@patch("secret_loader.cli.secret")
+@patch("secret_loader.secrets.secret")
 def test_secret_loader_cli_fail_silently(mock_secret, capsys):
     secret_name = "secret_name"
     secret_value = "secret_value"
     mock_secret.side_effect = secrets.SecretNotFoundError()
-
-    class MockArgs:
-        def __init__(self, name, fail=False):
-            self.name = name
-            self.fail = fail
 
     args = MockArgs(secret_name)
     cli.secret_loader_cli(args)
@@ -70,18 +91,13 @@ def test_secret_loader_cli_fail_silently(mock_secret, capsys):
     assert cleaned_output == ""
 
 
-@patch("secret_loader.cli.secret")
+@patch("secret_loader.secrets.secret")
 def test_secret_loader_cli_fail(mock_secret, capsys):
     secret_name = "secret_name"
     secret_value = "secret_value"
     mock_secret.side_effect = secrets.SecretNotFoundError()
 
-    class MockArgs:
-        def __init__(self, name, fail=True):
-            self.name = name
-            self.fail = fail
-
-    args = MockArgs(secret_name)
+    args = MockArgs(secret_name, fail=True)
     with pytest.raises(secrets.SecretNotFoundError):
         cli.secret_loader_cli(args)
 
@@ -93,3 +109,26 @@ def test_cli(mock_secret_loader_cli):
 
     assert mock_secret_loader_cli.called
     mock_secret_loader_cli.assert_called_with(args=argument)
+
+
+def test_available_loader_count():
+    assert 2 < len(cli.available_loaders)
+
+
+def test_get_secret_loader_for_specific_loader():
+    loader_class = list(cli.available_loaders)[0]
+    args = MockArgs("some_name", loader=loader_class)
+    with patch.object(secrets.SecretLoader, "register", return_value=None) as mock_register:
+        secret = cli.get_secret_loader(args)
+
+    assert mock_register.called
+    mock_register.assert_called_once_with(cli.available_loaders[loader_class])
+
+
+def test_get_secret_loader_without_specifying_loader():
+    args = MockArgs("some_name")
+    with patch.object(secrets.SecretLoader, "register", return_value=None) as mock_register:
+        secret = cli.get_secret_loader(args)
+
+    assert not mock_register.called
+    assert secret is secrets.secret
