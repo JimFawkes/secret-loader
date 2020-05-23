9cc29a99c3789d1292034f421ee5054c701ac620
Jim Fawkes <JimFawkes@users.noreply.github.com>
2020-05-23 22:57:21 +0200

    Add configuration for flit to pyproject.toml

--- secret_loader/__init__.py
+++ secret_loader/__init__.py
@@ -1,5 +1,18 @@
-# Copyright: (c) 2020, Moritz Eilfort
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+Flexible Secret Loader to load secrets from any source with a simple API!
+
+Copyright: (c) 2020, Moritz Eilfort
+GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+"""
+
+__version__ = "0.3"
+
+from . import loaders
+from . import base
+from . import exceptions
+from . import secrets
+
+secret = secrets.secret
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
